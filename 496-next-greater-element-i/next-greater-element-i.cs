3e5e1a90a339e2ee979e56e7be5282f42b271ce6
MdFarhanHridoy <88825030+MdFarhanHridoy@users.noreply.github.com>
2025-09-03 16:21:24 +0600

    Time: 1 ms (99.67%) | Memory: 47.9 MB (51.75%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        Dictionary<int, int> nextGreater = new Dictionary<int, int>();
+        Stack<int> stack = new Stack<int>();
+        
+        foreach (int num in nums2) {
+            while (stack.Count > 0 && stack.Peek() < num) {
+                nextGreater[stack.Pop()] = num;
+            }
+            stack.Push(num);
+        }
+        
+        while (stack.Count > 0) {
+            nextGreater[stack.Pop()] = -1;
+        }
+        
+        int[] result = new int[nums1.Length];
+        for (int i = 0; i < nums1.Length; i++) {
+            result[i] = nextGreater[nums1[i]];
+        }
+        
+        return result;
+    }
+}
